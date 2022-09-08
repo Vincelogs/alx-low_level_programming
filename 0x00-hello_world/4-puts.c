210b90d2e22c87414c5d0e7c42fdbd18dd458a1d
Vincent Obaro <vincentharyor@gmail.com>
2022-09-08 15:15:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return - Always 0
+ * Return: Always 0 
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
