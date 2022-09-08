ea3d081c39909d9fa6d425fac1a66e674da7f239
Vincent Obaro <vincentharyor@gmail.com>
2022-09-08 16:14:26 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
