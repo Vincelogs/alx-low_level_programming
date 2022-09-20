087e61bd9c46f919bb4283ddf88392a7ce067603
Vincent Obaro <vincentharyor@gmail.com>
2022-09-20 10:11:14 -0700

    prints string in reverse

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -7,3 +9,5 @@ int _strlen(char *s);
 void _puts(char *str);
 
 int _putchar(char c);
+
+void print_rev(char *s);
