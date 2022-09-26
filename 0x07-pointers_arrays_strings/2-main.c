690f797d61d97dfba37aebdfe29ce867f55584a9
Vincent Obaro <vincentharyor@gmail.com>
2022-09-26 14:19:44 -0700

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - checks the code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char *s = "hello";
+	char *f;
+
+	f = _strchr(s, 'l');
+
+	if (f != NULL)
+	{
+		printf("%s\n", f);
+	}
+	return (0);
+}
