f25be13097240d903aac25363e2b862ffb364b21
Vincent Obaro <vincentharyor@gmail.com>
2022-10-10 14:31:04 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
+#endif /* _DOG_H_ */
