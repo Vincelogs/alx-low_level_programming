cd75b95cbcff77eeb33b6381716f911d13ad3f42
Vincent Obaro <vincentharyor@gmail.com>
2022-10-13 06:31:36 -0700

    task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+#include <stdio.h>
+#include <stddef.h>
+
+int _putchar(char);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* _FUNCTION_POINTERS_ */
