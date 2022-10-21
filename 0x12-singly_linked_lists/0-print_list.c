c051e55280921632e380c65e65396fe7835aab3f
Vincent Obaro <vincentharyor@gmail.com>
2022-10-21 04:35:48 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the start of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *tmp;
+	unsigned int i;
+
+	tmp = h;
+	for (i = 0; tmp; i++)
+	{
+		if (tmp->str != NULL)
+			printf("[%u] %s\n", tmp->len, tmp->str);
+		else
+			printf("[0] (nil)\n");
+		tmp = tmp->next;
+	}
+	return (i);
+}
