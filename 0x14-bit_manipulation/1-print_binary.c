076b42398ded862d1086dd245c82345b144abb06
Vincent Obaro <vincentharyor@gmail.com>
2022-10-29 14:29:40 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_binary - Print binary representation of number
+ * @n: The number
+ *
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int i, start = 0;
+	unsigned long int num_bits, mask, result;
+
+	num_bits = sizeof(unsigned long int) * 8 - 1;
+	mask = (unsigned long int)1 << num_bits;
+	for (i = 0; i < num_bits; i++)
+	{
+		if (i == (num_bits - 1))
+			mask = 1;
+		if ((n & mask) > 0)
+			start = 1;
+		result = n & mask;
+		if (start)
+		{
+			if (result > 0)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+		if ((i == (num_bits - 1)) && (start == 0))
+			_putchar('0');
+		mask >>= 1;
+	}
+}
